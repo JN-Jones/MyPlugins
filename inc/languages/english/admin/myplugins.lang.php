f7aba969ce2ddee8b98aff78333ac44bf9bce8c3
JN-Jones <jones_h1@web.de>
2013-04-24 11:23:52 +0200

    Added english as language

--- inc/languages/english/admin/myplugins.lang.php
+++ inc/languages/english/admin/myplugins.lang.php
@@ -2,31 +2,31 @@
 $l['myplugins'] = "MyPlugins";
 
 //Admin Menu
-$l['myplugins_overview'] = "Übersicht";
-$l['myplugins_stats'] = "Anonyme Statistiken";
-$l['myplugins_verification'] = "Dateiüberprüfung";
+$l['myplugins_overview'] = "Overview";
+$l['myplugins_stats'] = "Anonymous Statistics";
+$l['myplugins_verification'] = "File Verification";
 
 //Admin Permissions
-$l['myplugins_permission_list'] = "Kann die Übersicht sehen";
-$l['myplugins_permission_stats'] = "Kann die Statistikeinstellungen &auml;ndern";
-$l['myplugins_permission_verification'] = "Kann die Dateiüberprüfung starten";
+$l['myplugins_permission_list'] = "Can see the overview";
+$l['myplugins_permission_stats'] = "Can change the settings for the statistics";
+$l['myplugins_permission_verification'] = "Can use the file verification";
 
-$l['active_plugin'] = "Aktive Plugins";
-$l['no_active_plugins'] = "Keine aktiven Plugins vorhanden";
-$l['inactive_plugin'] = "Deaktvierte Plugins";
-$l['no_inactive_plugins'] = "Keine deaktivierten Plugins vorhanden";
-$l['downloadable_plugin'] = "Plugins zum downloaden";
-$l['no_downloadable_plugins'] = "Keine downloadbaren Plugins vorhanden";
-$l['no_connection'] = "Keine Verbindung zum Server möglich";
+$l['active_plugin'] = "Activated Plugins";
+$l['no_active_plugins'] = "You have no plugins activated";
+$l['inactive_plugin'] = "Deactivated Plugins";
+$l['no_inactive_plugins'] = "You have no plugins deactivated";
+$l['downloadable_plugin'] = "Download Plugins";
+$l['no_downloadable_plugins'] = "You can't download any new plugins";
+$l['no_connection'] = "Can't connect to the server";
 $l['md5'] = "MD5";
-$l['missing'] = "Fehlt";
-$l['changed'] = "Bearbeitet";
+$l['missing'] = "Missing";
+$l['changed'] = "Edited";
 
-$l['save'] = "Speichern";
-$l['activate'] = "Aktivieren";
-$l['myplugins_activate'] = "Sollen Anonyme Statistiken an JonesBoard &uuml;bermittelt werden?";
-$l['myplugins_activate_desc'] = "&Uuml;bermittelt werden nur ServerInformationen, sowie &uuml;ber die von mir erstellten Plugins. Es werden KEINE weiteren Infos &uuml;ber dein Board &uuml;bermittelt.";
-$l['myplugins_code'] = "Statistik Code";
-$l['myplugins_code_desc'] = "Dieser zufallsgenerierte Code wird benutzt um die gesendeten Daten verarbeiten zu k&ouml;nnen.<br />
-Wenn du diesen Code auf JonesBoard in deinem <a href=\"http://jonesboard.de/usercp.php?action=myplugins\" target=\"_blank\">UserCP</a> eintr&auml;gst, kann dir noch besser Support geleistet werden.";
+$l['save'] = "Save";
+$l['activate'] = "Activate";
+$l['myplugins_activate'] = "Send anonymous statistics to JonesBoard?";
+$l['myplugins_activate_desc'] = "Just server information and information about my plugins will be send. NO other information over your board will be transmitted.";
+$l['myplugins_code'] = "Statistic Code";
+$l['myplugins_code_desc'] = "This randomly generated code is used to handle the data.<br />
+If you enter this Code in your <a href=\"http://jonesboard.de/usercp.php?action=myplugins\" target=\"_blank\">UserCP</a> at JonesBoard, your support will be better.";
 ?>
